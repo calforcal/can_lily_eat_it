c755c003551955cd77cbf0b001f37276be879a89
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-01-29 17:55:12 -0700

    Add AllergensFacade to handle API call with UPC information

---
+++ app/facades/allergens_facade.rb
@@ -0,0 +1,10 @@
+class AllergensFacade
+  def get_allergen_by_upc(upc)
+    details = service.get_allergen_info(upc)
+    Allergen.new(details)
+  end
+
+  def service
+    AllergensService.new
+  end
+end
