673753da70bc3bef5066d4256685ce006f970ff5
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-12 07:28:24 -0600

    Add Users#create route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :allergens, only: %i[index show]
+      resources :users, only: %i[create]
     end
   end
 end
