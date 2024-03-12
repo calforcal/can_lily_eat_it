9c22e5864d0a88afaedd54a8e0d7ef27d63fec4b
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-12 07:42:09 -0600

    Fix missing do end in specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-RSpec.describe Users, :model do
-  describe "validations"
+RSpec.describe User, :model do
+  describe "validations" do
     it { should validate_presence_of :name }
     it { should have_secure_password :password }
     it { should validate_uniqueness_of :email }
