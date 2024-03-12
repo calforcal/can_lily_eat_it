16f62199bb005e0d73a70a7913d2203cd1c6d6b1
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-12 07:27:28 -0600

    Add UsersController with #create action

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.create!(user_params)
+    render json: UserSerializer.new(user), status: :created
+  end
+
+  private
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+end
