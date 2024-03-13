75c35c35507fc69933bad560627beef35ff5c575
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-13 08:50:43 -0600

    Add associations and relationshops between User, Food and UserFood

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,8 @@
 class Food < ApplicationRecord
-  validates_presence_of :name, :upc_code, :ingredients, :allergens, :lily_eat
+  validates_presence_of :name, :upc_code
+  validates :lily_eat,
+      :inclusion => {:in => [true, false], :message => "can't be blank"}
+
+  has_many :user_foods
+  has_many :users, through: :user_foods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :name, :password
   validates_uniqueness_of :email
+
+  has_many :user_foods
+  has_many :foods, through: :user_foods
 end
