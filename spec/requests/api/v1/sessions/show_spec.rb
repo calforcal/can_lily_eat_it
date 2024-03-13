35a025e95981837287199513300d02e26cb539cc
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-13 07:28:49 -0600

    Add test specs for retaining a logged-in users session

---
+++ spec/requests/api/v1/sessions/show_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "Current Session" do
+  describe "#show" do
+    describe "#happy paths" do
+      it "can get a logged-in users current session" do
+        created_user = User.create!(name: "mickey", email: "crazy_unique_email@gmail.com", password: "mickey123", password_confirmation: "mickey123")
+
+        json_body = {
+          "name": "mickey",
+          "email": "crazy_unique_email@gmail.com",
+          "password": "mickey123"
+        }
+  
+        post api_v1_sessions_path(json_body)
+  
+        expect(response).to be_successful 
+        parsed = JSON.parse(response.body, symbolize_names: true)  
+        user = parsed[:data]
+
+        get "/api/v1/logged_in"
+
+        expect(response).to be_successful 
+        parsed = JSON.parse(response.body, symbolize_names: true)  
+        logged_in_user = parsed[:data]
+
+        expect(logged_in_user).to have_key(:id)
+        expect(logged_in_user).to have_key(:type)
+        expect(logged_in_user[:type]).to eq("user")
+        expect(logged_in_user).to have_key(:attributes)
+  
+        expect(logged_in_user[:attributes]).to have_key(:name)
+        expect(logged_in_user[:attributes][:name]).to eq("mickey")
+        expect(logged_in_user[:attributes]).to have_key(:email)
+        expect(logged_in_user[:attributes][:email]).to eq("crazy_unique_email@gmail.com")
+      end
+    end
+  end
+end
