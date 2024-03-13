d438f9f15cb0901f9ddaf9b683d279e755785b69
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-13 07:31:07 -0600

    Add #show action to Sessions for retaining/displaying a logged-in users session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,10 +2,15 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if !user.nil? && user.authenticate(params[:password])
+      session[:user_id] = user.id
       render json: UserSerializer.new(user).serialize_user, status: :created
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
+  end
+
+  def show
+    render json: UserSerializer.new(@current_user).serialize_user, status: :ok
   end
 
   def destroy
