29ebb938e3a1ecf6006e76a3e11c42e0cb6da0e6
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-14 08:21:59 -0600

    Add #create action in FoodController and strong params, allowing for arrays

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -5,8 +5,17 @@ class Api::V1::FoodsController < ApplicationController
     render json: FoodSerializer.new.serialize_all_foods(foods), status: :ok
   end
 
+  def create
+    new_food = @user.foods.create!(food_params)
+    render json: FoodSerializer.new.serialize_one_food(new_food), status: :created
+  end
+
   private
   def get_user
     @user = User.find(params[:user_id])
+  end
+
+  def food_params
+    params.permit(:name, :upc_code, :lily_eat, ingredients:[], allergens:[])
   end
 end
