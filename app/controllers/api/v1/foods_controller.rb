8465dfe3800afb910b454c89db1ff8a29b93c0a2
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-14 07:33:40 -0600

    Add json response using FoodSerializer method for a Users Foods

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::FoodsController < ApplicationController
   before_action :get_user
   def index
-    
+    foods = @user.foods
+    render json: FoodSerializer.new.serialize_all_foods(foods), status: :ok
   end
 
   private
   def get_user
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 end
