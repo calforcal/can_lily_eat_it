f0757823b459fb68c3764778729409d543d27e62
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-14 07:33:02 -0600

    Add #serialize_all_foods method to FoodSerializer

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -16,4 +16,22 @@ class FoodSerializer
       }
     }
   end
+
+  def serialize_all_foods(foods)
+    {
+      "data": foods.map do |food|
+        {
+          "id": food.id,
+          "type": "food",
+          "attributes": {
+            "name": food.name,
+            "upc_code": food.upc_code,
+            "allergens": food.allergens,
+            "ingredients": food.ingredients,
+            "lily_eat": food.lily_eat
+          }
+        }
+      end
+    }
+  end
 end
