7f97d5e14bf95f22cfcf7994aefded2e8e7c8165
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-14 08:53:55 -0600

    Add route for UserFoods #destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :allergens, only: %i[index show]
       resources :users, only: %i[create show] do
-        resources :foods, only: %i[index create]
+        resources :foods, only: %i[index create destroy]
       end
       resources :sessions, only: %i[create]
 
