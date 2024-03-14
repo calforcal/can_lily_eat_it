628405a52eecda21c6084d090dbe84b56e788afb
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-14 08:53:24 -0600

    Add model method for UserFood #find_by_user_and_food_id, to delete joins table record

--- app/models/user_food.rb
+++ app/models/user_food.rb
@@ -1,4 +1,8 @@
 class UserFood < ApplicationRecord
   belongs_to :user
   belongs_to :food
+
+  def self.find_by_user_and_food_id(user_id, food_id)
+    where(user_id: user_id).where(food_id: food_id).first
+  end
 end
