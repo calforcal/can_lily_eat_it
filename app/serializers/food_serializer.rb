ff2ab2a60f9c56baf755d14a4abe0e3f572172b4
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-14 08:22:38 -0600

    Add conditional response to #serialize_one_food, based on API call or DB object

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -4,18 +4,34 @@ class FoodSerializer
   end
 
   def serialize_one_food(food)
-    {
-      "data": {
-        "type": "food",
-        "attributes": {
-          "name": food.name,
-          "upc_code": food.upc_code,
-          "allergens": food.allergens,
-          "ingredients": food.ingredients,
-          "lily_eat": food.lily_eat
+    if food.id != nil
+      {
+        "data": {
+          "id": food.id,
+          "type": "food",
+          "attributes": {
+            "name": food.name,
+            "upc_code": food.upc_code,
+            "allergens": food.allergens,
+            "ingredients": food.ingredients,
+            "lily_eat": food.lily_eat
+          }
         }
       }
-    }
+    else
+      {
+        "data": {
+          "type": "food",
+          "attributes": {
+            "name": food.name,
+            "upc_code": food.upc_code,
+            "allergens": food.allergens,
+            "ingredients": food.ingredients,
+            "lily_eat": food.lily_eat
+          }
+        }
+      }
+    end
   end
 
   def serialize_all_foods(foods)
