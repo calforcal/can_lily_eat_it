7b88449b6e15a683d483f7bcb89c8dd3da081719
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-14 07:51:47 -0600

    Update AllergenFacade to pass through UPC code to Allergen POROS

--- app/facades/allergens_facade.rb
+++ app/facades/allergens_facade.rb
@@ -1,7 +1,7 @@
 class AllergensFacade
   def get_allergen_by_upc(upc)
     details = service.get_allergen_info(upc)
-    Allergen.new(details)
+    Allergen.new(details, upc)
   end
 
   def service
