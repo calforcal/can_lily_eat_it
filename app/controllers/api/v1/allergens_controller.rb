e55d4ddddf127c6f9c1a0976d6543199e28d5dea
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-28 17:28:36 -0600

    Add functionality to support UPC not found searches, return 204 no response to Frontend

--- app/controllers/api/v1/allergens_controller.rb
+++ app/controllers/api/v1/allergens_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::AllergensController < ApplicationController
   def index
     allergen = AllergensFacade.new.get_allergen_by_upc(params[:upc])
-    render json: FoodSerializer.new.serialize_one_food(allergen)
+    if allergen[:message]
+      render json: allergen, status: 204
+    else
+      render json: FoodSerializer.new.serialize_one_food(allergen)
+    end
   end
 end
--- app/facades/allergens_facade.rb
+++ app/facades/allergens_facade.rb
@@ -1,7 +1,13 @@
 class AllergensFacade
   def get_allergen_by_upc(upc)
     details = service.get_allergen_info(upc)
-    Allergen.new(details, upc)
+    if details[:success] == false
+      {
+        "message": "Not Found. No product could be found with that code."
+      }
+    else
+      Allergen.new(details, upc)
+    end
   end
 
   def service
