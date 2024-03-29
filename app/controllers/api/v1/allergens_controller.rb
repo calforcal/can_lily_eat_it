482f40eaf568ccad3b8d29d83286a4d284ef6c19
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-28 18:05:17 -0600

    Refactor the Allergens Controller to better handle not found foods

--- app/controllers/api/v1/allergens_controller.rb
+++ app/controllers/api/v1/allergens_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::AllergensController < ApplicationController
   def index
     allergen = AllergensFacade.new.get_allergen_by_upc(params[:upc])
-    if allergen[:message]
-      render json: allergen, status: 204
-    else
+    if allergen.is_a?(Allergen)
       render json: FoodSerializer.new.serialize_one_food(allergen)
+    else
+      render json: allergen, status: 204
     end
   end
 end
