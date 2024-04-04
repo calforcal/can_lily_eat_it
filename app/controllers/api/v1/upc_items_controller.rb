7004bf2441b8dde2a2bd4c469e93311a3faf6e6c
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-04 09:27:59 -0600

    Add logic to handle allergens params or find users allergens when searching a UPC code

--- app/controllers/api/v1/upc_items_controller.rb
+++ app/controllers/api/v1/upc_items_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::UpcItemsController < ApplicationController
   def index
-    item = UpcItemsFacade.new.get_item_by_upc(params[:upc])
+    if current_user?
+      allergens = @current_user.get_user_allergens
+    else
+      merged_allergens = {}
+      params[:allergens].each { |allergen| merged_allergens.merge!(Allergen.find_by(name: allergen).found_in) }
+      allergens = merged_allergens
+    end
+
+    item = UpcItemsFacade.new.get_item_by_upc(params[:upc], allergens)
     if item.is_a?(UpcItem)
       render json: FoodSerializer.new.serialize_one_food(item)
     else
