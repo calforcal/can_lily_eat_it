f78e8845cb8658b0d22b106b800b47384253c556
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-04 08:19:58 -0600

    Add User instance method for combining user allergens, used in can_lily_eat field on API hit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   has_many :foods, through: :user_foods
   has_many :user_allergens
   has_many :allergens, through: :user_allergens
+
+  def get_user_allergens
+    merged_allergens = {}
+    allergens.each { |allergen| merged_allergens.merge!(allergen.found_in) }
+    merged_allergens
+  end
 end
