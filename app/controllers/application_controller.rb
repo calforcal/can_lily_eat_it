8fa162af75bf264abae6b069e987a31da2234db4
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-04 09:27:20 -0600

    Add current_user? method to determine if someone is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::API
   def set_current_user
    if session[:user_id]
     @current_user = User.find(session[:user_id])
+   else
+    @current_user = nil
    end
+  end
+
+  def current_user?
+    @current_user == nil ? false : true
   end
 end
