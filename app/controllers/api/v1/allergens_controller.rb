36fe1a31c55f8e8d40b713b1405a1a7658e41a11
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-06 12:00:51 -0600

    Add UserAllergens#update action in controller to verify existing UserAllergens and create, destroy or do nothing depending on params

--- app/controllers/api/v1/allergens_controller.rb
+++ app/controllers/api/v1/allergens_controller.rb
@@ -11,6 +11,34 @@ class Api::V1::AllergensController < ApplicationController
     render json: { message: "Your selections have been saved." }, status: 201
   end
 
+  def update
+    allergen_hash = {
+      "dairy" => false,
+      "soy" => false,
+      "fish" => false,
+      "shellfish" => false,
+      "treenut" => false,
+      "peanut" => false,
+      "wheat" => false,
+      "egg" => false
+    }
+
+    params[:allergens].split(",").each { |allergen| allergen_hash[allergen] = true }
+    
+    allergen_hash.to_a.each do |arr|
+      found_allergen = Allergen.find_by(name: arr[0])
+      user_allergen = UserAllergen.find_by(user_id: @user.id, allergen_id: found_allergen.id)
+
+      if arr[1] == true && user_allergen == nil
+        UserAllergen.create!(user_id: @user.id, allergen_id: found_allergen.id)
+      elsif arr[1] == false && user_allergen != nil
+        UserAllergen.find_by(user_id: @user.id, allergen_id: found_allergen.id).destroy
+      end
+    end
+
+    render json: { message: "Your selections have been saved." }, status: 200
+  end
+
   private
   def get_user
     @user = User.find(params[:user_id])
