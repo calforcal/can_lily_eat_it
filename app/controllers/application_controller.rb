fe4e5bffefe8def53262d1954929bbebfb6a9757
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-12 10:42:24 -0600

    Fix set_current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  
   before_action :set_current_user
   def set_current_user
-   if session[:user_id]
-    @current_user = User.find(session[:user_id])
-   else
-    @current_user = nil
-   end
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
   end
 
   def current_user?
