0f2fae885ffd6388646e3792ee6531aadf4dbc0a
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-17 09:17:06 -0600

    Update CORS Access Link to correct Live deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://127.0.0.1:5173",
             "http://localhost:5173",
-            "https://can-lily-eat-it-ui.vercel.app/"
+            "https://can-lily-eat-it-ui.vercel.app"
 
     resource "*",
       headers: :any,
