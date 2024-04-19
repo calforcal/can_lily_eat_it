90c6e19ac7b775394d7a52011c6ca68f98370eff
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-19 10:48:30 -0600

    Update testing specs for #create and #show user to use sessions

--- spec/requests/api/v1/users/show_spec.rb
+++ spec/requests/api/v1/users/show_spec.rb
@@ -6,13 +6,20 @@ RSpec.describe "User by ID" do
       it "can get a users information by ID" do
         created_user = User.create!(name: "mickey", email: "crazy_unique_email@gmail.com", password: "mickey123", password_confirmation: "mickey123")
   
-        get api_v1_user_path(created_user.id)
+        post api_v1_sessions_path({ email: created_user.email, password: created_user.password })
+        expect(response).to be_successful 
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        returned_user = parsed[:data]
+  
+        headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization': returned_user[:token]}
+        get api_v1_user_path(created_user.id), headers: headers
   
         expect(response).to be_successful
         parsed = JSON.parse(response.body, symbolize_names: true)  
         user = parsed[:data]
 
         expect(user).to have_key(:id)
+        expect(user).to have_key(:token)
         expect(user).to have_key(:type)
         expect(user[:type]).to eq("user")
         expect(user).to have_key(:attributes)
